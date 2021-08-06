400e086c0540f3f78f5a88314158c7d633ff6e67
Mohnish Jindal <mohnishjindal2004@gmail.com>
2021-08-06 17:39:38 +0530

    Working commit

---
+++ stockquotes/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from .models import Stock
+from .forms import StockForm
+from django.contrib import messages
+# pk_d7e0b5aef0964361983c75c60032ea92
+def home(request):
+    import requests 
+    import json
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_d7e0b5aef0964361983c75c60032ea92")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render(request, 'home.html', {'api': api})
+
+    else: 
+        return render(request, 'home.html', {'ticker': "Enter a stock symbol above.."})
+    
+
+def about(request):
+    return render(request, 'about.html', {})
+def add_stock(request):
+    import requests 
+    import json
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Stock has been added!"))
+            return redirect('add_stock')
+    else:
+
+        ticker = Stock.objects.all()
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_d7e0b5aef0964361983c75c60032ea92")
+
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output} )
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been Deleted!"))
+    return redirect(add_stock)
